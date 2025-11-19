dfc360a703f164deb0a595fc9dc96b4ce95989f4
SleepyWolvy <sangbo2001@gmail.com>
2025-11-19 14:55:35 +0900

    ✨Detail View

--- S25W12Navigation/ContentView.swift
+++ S25W12Navigation/ContentView.swift
@@ -1,21 +1,64 @@
-//
-//  ContentView.swift
-//  S25W12Navigation
-//
-//  Created by Student on 11/17/25.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var viewModel = SongViewModel()
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List(viewModel.songs) { song in
+                NavigationLink(destination: SongDetailView(song: song)) {
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(song.title)
+                                    .font(.headline)
+                                Text(song.singer)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                        }
+                    }
+            }
         }
-        .padding()
+    }
+}
+
+//struct SingerView: View {
+//    var body: some View {
+//        NavigationStack {
+//            Text("가수 화면")
+//
+////            NavigationLink(destination: SingerView()) {
+////                Text("노래 화면으로 이동")
+////            }
+//        }
+//    }
+//}
+
+struct SongDetailView: View {
+    let song: Song
+
+    var body: some View {
+        ScrollView {
+            VStack(alignment: .leading, spacing: 15) {
+                HStack {
+                    Text(song.singer)
+                        .font(.title2)
+                        .foregroundColor(.secondary)
+                    Spacer()
+                    Text(String(song.rating))
+                        .font(.title)
+                        .foregroundColor(.yellow)
+                }
+                .padding(.bottom, 10)
+
+                Text(song.lyrics)
+                    .font(.body)
+                    .multilineTextAlignment(.leading)
+            }
+            .padding()
+        }
+        .navigationTitle(song.title)
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
